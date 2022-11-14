adb1f8869120c8097e892b39da34cdc6628fb6c1
gwkim <youlive789@gmail.com>
2022-11-14 21:05:34 +0900

    feat: add animation example

--- lib/service/game/Player.dart
+++ lib/service/game/Player.dart
@@ -1,28 +1,88 @@
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'game_screen.dart';
 
-class Player extends SpriteComponent with Draggable, HasGameRef<MyGame> {
+class Player extends SpriteAnimationComponent with HasGameRef<MyGame> {
   Player({
     required Vector2 size,
     required Vector2 position,
   }) : super(size: size, position: position);
 
+  final double _animationSpeed = 0.15;
+  late final SpriteAnimation _runDownAnimation;
+  late final SpriteAnimation _runLeftAnimation;
+  late final SpriteAnimation _runUpAnimation;
+  late final SpriteAnimation _runRightAnimation;
+  late final SpriteAnimation _standingAnimation;
+
   @override
   Future<void> onLoad() async {
-    sprite = await Sprite.load("sample.png");
+    final spriteSheet = SpriteSheet(
+        image: await gameRef.images.load("example.png"),
+        srcSize: Vector2(29.0, 32.0));
+
+    _runDownAnimation = spriteSheet.createAnimation(
+      row: 0,
+      stepTime: _animationSpeed,
+      to: 3,
+    );
+
+    _runLeftAnimation = spriteSheet.createAnimation(
+      row: 1,
+      stepTime: _animationSpeed,
+      to: 3,
+    );
+
+    _runUpAnimation = spriteSheet.createAnimation(
+      row: 2,
+      stepTime: _animationSpeed,
+      to: 3,
+    );
+
+    _runRightAnimation = spriteSheet.createAnimation(
+      row: 3,
+      stepTime: _animationSpeed,
+      from: 0,
+      to: 4,
+    );
+
+    _standingAnimation = spriteSheet.createAnimation(
+      row: 0,
+      stepTime: _animationSpeed,
+      to: 1,
+    );
+
+    animation = _standingAnimation;
   }
 
   @override
-  bool onDragUpdate(DragUpdateInfo info) {
-    position.x = info.eventPosition.game.x;
-    position.y = info.eventPosition.game.y;
-    return true;
+  void render(Canvas canvas) {
+    super.render(canvas);
   }
 
   @override
   void update(double dt) {
-    position += gameRef.joystick.delta * 0.1;
+    super.update(dt);
+    position += gameRef.joystick.delta * 0.05;
+
+    switch (gameRef.joystick.direction) {
+      case JoystickDirection.up:
+        animation = _runUpAnimation;
+        break;
+      case JoystickDirection.down:
+        animation = _runDownAnimation;
+        break;
+      case JoystickDirection.left:
+        animation = _runLeftAnimation;
+        break;
+      case JoystickDirection.right:
+        animation = _runRightAnimation;
+        break;
+      default:
+        animation = _standingAnimation;
+        break;
+    }
   }
 }
--- lib/service/game/game_screen.dart
+++ lib/service/game/game_screen.dart
@@ -8,6 +8,7 @@ import 'package:game/service/game/joystick.dart';
 
 class MyGame extends FlameGame with HasDraggables {
   late Joystick joystick;
+  late Player player;
 
   @override
   Future<void> onLoad() async {
@@ -17,16 +18,17 @@ class MyGame extends FlameGame with HasDraggables {
         paint: BasicPalette.white.paint());
     add(background);
 
-    var player = Player(size: Vector2(30, 40), position: Vector2(100, 100));
-    add(player);
-
     var joystickOutside = JoystickOutside(size: Vector2(100, 100));
     var joystickInside = JoystickInside(size: Vector2(50, 50));
     joystick = Joystick(
         knob: joystickInside,
         background: joystickOutside,
-        position: Vector2(100, 500));
+        position: Vector2(size.x * 0.5, size.y * 0.75));
     add(joystick);
+
+    player = Player(
+        size: Vector2(30, 40), position: Vector2(size.x * 0.5, size.y * 0.5));
+    add(player);
   }
 
   @override
